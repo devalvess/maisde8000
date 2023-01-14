40b57fa89fb9d609c147687253dd7b13b90729b7
Edilson Santos <edilsonsantosexsp@gmail.com>
2023-01-14 10:43:13 -0300

    first commit

---
+++ app/Http/Controllers/EnergyController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EnergyController extends Controller
+{
+    public function dbz($id, Request $request)
+    {
+        $energy = $id;
+
+        if($energy > 8000) 
+        { $energy = "Mais de 8000!";}
+        else 
+        { $energy = "Inseto!";}
+
+        return view('/dbz', compact('energy'));
+            
+    } 
+
+}    
